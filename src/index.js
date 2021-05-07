05165081696318c2ad0d88aa57adcc8e1593ed8a
Ben Ooms <benoomsdh@gmail.com>
2021-05-07 16:00:19 +0200

    Replaced old app name

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ ReactDOM.render(
   <React.StrictMode>
     <ClockApp />
   </React.StrictMode>,
-  document.getElementById('react-calculator')
+  document.getElementById('pomodoro-app')
 );
 
 // If you want to start measuring performance in your app, pass a function
