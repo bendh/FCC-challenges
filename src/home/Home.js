e394d40eab5dc99cf0f92552969a0722098ee98b
Ben Ooms <benoomsdh@gmail.com>
2021-05-17 21:49:56 +0200

    Added Material UI

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Button from '@material-ui/core/Button';
 
 export class HomePage extends React.Component {
   render() {
-    return (<><p>Home is working</p></>);
+    return (
+    <>
+      <p>Home is working</p>
+      <Button variant="contained" color="primary" >Hello</Button>
+    </>);
   }
 }
